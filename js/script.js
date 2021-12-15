16ec6e98537e81cf3eeade72c16d038bb2b47c80
Yantis <yantis1212@outlook.com>
2021-12-14 20:07:31 -0700

    Game Complete

--- js/script.js
+++ js/script.js
@@ -5,6 +5,25 @@
 let playerScore = 0;
 let computerScore = 0;
 
+function newGame() {
+    modal.classList.remove("win");
+    modal.classList.remove("lose");
+    cpuRockBtn.classList.remove("cpu__choice");
+    cpuPaperBtn.classList.remove("cpu__choice");
+    cpuScissorsBtn.classList.remove("cpu__choice");
+    playerRockBtn.classList.remove("cpu__choice");
+    playerPaperBtn.classList.remove("cpu__choice");
+    playerScissorsBtn.classList.remove("cpu__choice");
+    playerScore = 0;
+    computerScore = 0;
+    pScore.textContent = playerScore;
+    cScore.textContent = computerScore;
+    modal.classList.add("hidden");
+    main.classList.remove("hidden");
+    gameMessage.classList.remove("hidden");
+    gameMessage.textContent = "";
+}
+
 // COMPUTER GUESS
 const guess = ["Rock", "Paper", "Scissors"];
 function computerPlay() {
@@ -15,14 +34,21 @@ function computerPlay() {
 
 //Start Round
 function startRound(playerSelect) {
+    cpuRockBtn.classList.remove("cpu__choice");
+    cpuPaperBtn.classList.remove("cpu__choice");
+    cpuScissorsBtn.classList.remove("cpu__choice");
+    playerRockBtn.classList.remove("cpu__choice");
+    playerPaperBtn.classList.remove("cpu__choice");
+    playerScissorsBtn.classList.remove("cpu__choice");
     let playerSelection = playerSelect;
     let computerSelection = computerPlay();
-    console.log(playerSelection);
-    console.log(computerSelection);
+    cpuAnimation(computerSelection);
+    playerAnimation(playerSelection);
     playRound(playerSelection, computerSelection);
     pScore.textContent = playerScore;
     cScore.textContent = computerScore;
 }
+//Play Round
 function playRound(playerSelection, computerSelection) {
     //Player Wins
     if (
@@ -42,21 +68,50 @@ function playRound(playerSelection, computerSelection) {
         gameMessage.textContent = `You Lose: ${computerSelection} beats ${playerSelection}`;
         computerScore++;
     }
+    winCheck();
 }
-// // 7. Create Game Reset
-
-// 7. Create loop to run game for 5 rounds
-// function game() {
-// 	for (let i = 0; i < 5; i++) {
-// 		computerPlay();
-// 		let computerSelection = guess[computerPlay()];
-// 		let playerSelection = prompt();
-// 		playRound(playerSelection, computerSelection);
-// 	}
-// }
-// game();
-
-// UI //////////////////////////////////////////////////////////////////
+//Win Check Function
+function winCheck() {
+    if (playerScore === 5) {
+        modal.classList.remove("hidden");
+        modal.classList.add("win");
+        main.classList.add("hidden");
+        gameMessage.textContent = "";
+        gameMessage.classList.add("hidden");
+        winMessage.textContent = `You Win the Game! ${playerScore} to ${computerScore}`;
+    } else if (computerScore === 5) {
+        modal.classList.remove("hidden");
+        modal.classList.add("lose");
+        main.classList.add("hidden");
+        gameMessage.classList.add("hidden");
+        gameMessage.textContent = "";
+        winMessage.textContent = `You Lose the Game! ${playerScore} to ${computerScore}`;
+    } else return;
+}
+// ANIMATION /////////////////////////
+//Computer Choice Animation
+function cpuAnimation(computerSelection) {
+    if (computerSelection === "Rock") {
+        cpuRockBtn.classList.add("cpu__choice");
+    }
+    if (computerSelection === "Paper") {
+        cpuPaperBtn.classList.add("cpu__choice");
+    }
+    if (computerSelection === "Scissors") {
+        cpuScissorsBtn.classList.add("cpu__choice");
+    }
+}
+function playerAnimation(playerSelection) {
+    if (playerSelection === "Rock") {
+        playerRockBtn.classList.add("cpu__choice");
+    }
+    if (playerSelection === "Paper") {
+        playerPaperBtn.classList.add("cpu__choice");
+    }
+    if (playerSelection === "Scissors") {
+        playerScissorsBtn.classList.add("cpu__choice");
+    }
+}
 
 // UI ELEMENT SELECT METHODS
 const playerRockBtn = document.getElementById("playerRockBtn");
@@ -68,8 +123,13 @@ const cpuScissorsBtn = document.getElementById("cpuScissorsBtn");
 const pScore = document.getElementById("player__score");
 const cScore = document.getElementById("cpu__score");
 const gameMessage = document.getElementById("game__message");
+const modal = document.getElementById("modal");
+const main = document.getElementById("main");
+const winMessage = document.getElementById("win_message");
+const newGameButton = document.getElementById("newgame");
 
 // UI EVENT LISTENERS
 playerRockBtn.addEventListener("click", () => startRound("Rock"));
 playerPaperBtn.addEventListener("click", () => startRound("Paper"));
 playerScissorsBtn.addEventListener("click", () => startRound("Scissors"));
+newGameButton.addEventListener("click", () => newGame());
